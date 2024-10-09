7d8bf438d77a8ded0f4c61492b9879d59e9b1092
xdamiansarnecki@gmail.com <xdamiansarnecki@gmail.com>
2024-10-09 19:41:07 +0200

    planet text fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          Hello Planet
         </a>
       </header>
     </div>
